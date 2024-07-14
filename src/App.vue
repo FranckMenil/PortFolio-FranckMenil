3913642a6d1ecefe0d30e48a69ab287e62c90407
FranckMenil <franckmenil@gmail.com>
2024-07-14 11:45:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/MyHeader.vue'
+import Footer from './components/MyFooter.vue'
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
