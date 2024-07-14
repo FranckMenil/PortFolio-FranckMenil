c219fec03bc3bdc4c5631e85d297cb5d5e122b5c
FranckMenil <franckmenil@gmail.com>
2024-07-14 11:56:25 +0200

    my header

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,56 @@
+<template>
+    <header>
+        <div class="photo">
+            <a href="#" style = "padding:0;">
+                <img src="/src/assets/pictures/FranckM.jpg" alt="Photo">
+            </a>
+        </div>  
+        <nav>
+            <ul>
+                <li><a href="#presentation">Présentation</a></li>
+                <li><a href="#creations">Projets</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: #fff;
+  
+  z-index: 1000; 
+}
+
+.photo img{
+    height: 250px;
+    border-radius: 50%;
+    box-shadow: 0 0 10px#dbc2a4;
+    cursor: pointer;
+    
+}
+
+ul , li , a {
+    display: flex;
+    color: white;
+    list-style: none;
+    text-decoration: none;
+    padding: 3em 3em 0em 0em;
+    font-size: 20px;
+}
+
+nav ul li a:hover {
+  text-decoration: underline;
+}
+
+</style>
