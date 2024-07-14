b148d9c6b72253a2b84535a7480b5e210d2861e2
FranckMenil <franckmenil@gmail.com>
2024-07-14 11:57:07 +0200

    my footer

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,47 @@
+<template>
+    <footer>
+    <div class="social-links">
+      <a href="https://github.com/votreprofil"><img src ="/src/assets/pictures/github.png" target="_blank"></a>
+      <a href="https://github.com/votreprofil"><img src ="/src/assets/pictures/linkedin.png" target="_blank"></a>
+      <a href="https://github.com/votreprofil"><img src ="/src/assets/pictures/X.jpg" target="_blank"></a>
+    </div>
+    <p>Dernière mise à jour : {{ lastUpdated }}</p>
+  </footer>
+
+</template>
+
+<script>
+    export default {
+  data() {
+    return {
+      lastUpdated: new Date().toLocaleDateString()
+    }
+  }
+}
+</script>
+
+<style scoped>
+footer {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: #fff;
+  border-top: solid gray;
+}
+.social-links img {
+    height: 50px;
+    align-items: center;
+}
+.social-links a {
+  color: #fff;
+  margin-right: 1rem;
+  text-decoration: none;
+  transition: color 0.3s;
+}
+
+.social-links a:hover {
+  color: #ddd;
+}
+</style>
